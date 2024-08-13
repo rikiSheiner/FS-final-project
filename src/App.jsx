58a0fe3dcce998d870e7f930c58abb3faee3e1d0
rikiSheiner <r0587860843@gmail.com>
2024-08-13 17:49:25 +0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import Welcome from "./components/Welcome";
 
 import "./App.css";
 
+
+// WELCOME בהמשך 
+// יעל  - 4 ראשונים 
+// ריקי - 4 אחרונים
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome></Welcome>}></Route>
+        
         <Route path="login" element={<Login />}></Route>
         <Route path="signup" element={<Signup />}></Route>
         <Route path="home" element={<Home user={user}></Home>}>
@@ -32,25 +38,5 @@ function App() {
   );
 }
 
-/*
-<Routes>
-        <Route path='/' element={<Welcome></Welcome>}></Route>
-        <Route path='login' element={<Login setUser={setUser}></Login>} />
-        <Route path='register' element={<Register setUser={setUser}></Register>} />
-        <Route path='registration-details' element={<RegistrationDetails setUser={setUser}></RegistrationDetails>} />
-
-        <Route path='users/:userId' element={<SharedWrap></SharedWrap>}>
-          <Route path='home' element={<Home user={user}></Home>}></Route>
-          <Route path='albums' element={<Albums />}>
-              <Route index element={<Albums />} />
-          </Route>
-          <Route path='albums/:albumId' element={<MyAlbum/>}></Route>
-          <Route path='posts' element={<Posts />}>
-              <Route index element={<Posts />} />
-          </Route>         
-          <Route path='todos' element={<Todos user={user}></Todos>}></Route>
-      </Route>
-    </Routes>
-*/
 
 export default App;

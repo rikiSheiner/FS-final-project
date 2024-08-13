a264b377b2e4c62b398455091e1dae5f0052b5ea
vaisberger <yaelw123@gmail.com>
2024-08-13 17:26:12 +0300

    13.8

---
+++ src/components/navBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+ function NavBar(){
+    return(
+        <>        
+        <nav className={classes.navbar}>
+        <ul className={classes.navLnk}>
+          <li className={classes.liLnk}>
+            <Link to="home" className={classes.lnk}>Home</Link>
+          </li>
+          <li className={classes.liLnk}>
+            <Link to="orderAppointmet" className={classes.lnk}>Posts</Link>
+          </li>
+          <li className={classes.liLnk}>
+            <Link to="orderNewCard" className={classes.lnk}>orderNewCard</Link>
+          </li>
+          <li className={classes.liLnk}>
+            <Link to="pharmacy" className={classes.lnk}>Pharmacy</Link>
+          </li>
+          <li className={classes.liLnk}>
+            <Link to="myReferrals" className={classes.lnk}>MyReferrals</Link>
+          </li>
+          <li className={classes.liLnk}>
+            <Link to="myPrescriptions" className={classes.lnk}>MyPrescriptions</Link>
+          </li>
+          <li className={classes.liLnk}>
+            <Link to="/login"className={classes.lnk} >LogOut</Link>
+          </li>
+        </ul>
+      </nav> 
+      </>
+
+    )
+ }
+ export default NavBar;

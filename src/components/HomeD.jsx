c56c68d646e92e9cd59a689692ad99dd34883444
vaisberger <yaelw123@gmail.com>
2024-08-30 10:21:42 +0300

    homed ready

--- src/components/HomeD.jsx
+++ src/components/HomeD.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-
+import DoctorNavB from './DoctorNavB';
 const HomeD = () => {
-
+    <>
+    <div><DoctorNavB /></div>
+    <div>
+        <Outlet />
+    </div>
+</>
 };
 export default HomeD;
